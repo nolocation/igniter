b35a4884b9944f2c931736142540527bdbe0cd7c
gagarine <perdrisat@gmail.com>
2007-12-17 23:41:13 +0000

    Initial commit of igniter theme. This is a starting theme like zen or base but with a different target. Igniter are minimal CSS and XHTML for poeple whe would like a "clean" theme. For keep CSS file smal and reanding fast, comment are not present to big.

---
+++ template.php
@@ -0,0 +1,75 @@
+<?php
+function igniter_regions() {
+  return array(
+    'left' => t('left sidebar'),
+    'right' => t('right sidebar'),
+    'navbar' => t('navigation bar'),
+    'content_top' => t('content top'),
+    'content_bottom' => t('content bottom'),
+    'pre_header' => t('pre header'),
+    'header' => t('header'),
+    'footer' => t('footer'),
+    'closure_region' => t('closure'),
+  );
+}
+
+/**
+ * Intercept template variables
+ *
+ * @param $hook
+ *   The name of the theme function being executed (name of the .tpl.php file)
+ * @param $vars
+ *   A copy of the array containing the variables for the hook.
+ * @return
+ *   The array containing additional variables to merge with $vars.
+ */
+function _phptemplate_variables($hook, $vars = array()) {
+  // Get the currently logged in user
+  global $user;
+
+  // Set a new $is_admin variable. This is determined by looking at the
+  // currently logged in user and seeing if they are in the role 'admin'. The
+  // 'admin' role will need to have been created manually for this to work this
+  // variable is available to all templates.
+  $vars['is_admin'] = in_array('admin', $user->roles);
+
+  switch ($hook) {
+    case 'page':
+      global $theme, $theme_key;
+
+      // If we're in the main theme
+      if ($theme == $theme_key) {
+        // Avoid IE5 bug that always loads @import print stylesheets
+       $vars['head'] = igniter_add_print_css($vars['directory'] .'/print.css');
+      }
+    break;
+   }
+   return $vars;
+ }
+
+/**
+ * Adds a print stylesheet to the page's $head variable.
+ *
+ * This is a work-around for a serious bug in IE5 in which it loads print
+ * stylesheets for screen display when using an @import method, Drupal's default
+ * method when using drupal_add_css().
+ *
+ * @param string $url
+ *   The URL of the print stylesheet
+ * @return
+ *   All the rendered links for the $head variable
+ */
+function igniter_add_print_css($url) {
+  global $base_path;
+  return drupal_set_html_head(
+    '<link'.
+    drupal_attributes(
+      array(
+        'rel' => 'stylesheet',
+        'href' => $base_path . $url,
+        'type' => 'text/css',
+        'media' => 'print',
+      )
+    ) ." />\n"
+  );
+}
