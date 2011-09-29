5786e707497ad4fd91c1ac6392181e91684043f0
Simon Perdrisat <perdrisat@gmail.com>
2011-09-29 14:48:03 +0300

    add google frame support and compatible to Edge

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -47,6 +47,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 <head profile="<?php print $grddl_profile; ?>">
 <title><?php print $head_title; ?></title>
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
 <?php print $styles; ?>
 <?php print $scripts; ?>
 </head>
