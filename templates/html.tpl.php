167b81be8f35c9012014c9133ef86c52abc64c32
Pauline Dupraz <duprazpauline@gmail.com>
2011-11-17 17:04:26 +0100

    print favicon

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -46,6 +46,7 @@
   "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 <head profile="<?php print $grddl_profile; ?>">
+<?php print $head; ?>
 <title><?php print $head_title; ?></title>
 <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
 <?php print $styles; ?>
